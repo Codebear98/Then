b9d47489ccad255d00cf681072c69d9cfe122397
“Henry <henry.hong@scmp.com>
2017-01-04 14:53:47 +0800

    Added watchOS framework target

---
+++ Then/Then.h
@@ -0,0 +1,19 @@
+//
+//  Then.h
+//  Then
+//
+//  Created by henry.hong on 4/1/2017.
+//  Copyright © 2017 Suyeol Jeon. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for Then.
+FOUNDATION_EXPORT double ThenVersionNumber;
+
+//! Project version string for Then.
+FOUNDATION_EXPORT const unsigned char ThenVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Then/PublicHeader.h>
+
+
